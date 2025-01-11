23ebcfe989fca93e609d2ccb429a5240b84d8052
vanguard1105 <vanguard951105@gmail.com>
2025-01-11 02:22:38 -0500

    feat: Pump fun token swap endpoint

--- src/routes/orderRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { addOrder, removeOrder, getOrders } from "../controllers/orderControllers";
-const router = Router();
-
-router.post("/getOrders", getOrders);
-router.post("/addOrder", addOrder);
-router.post("/removeOrder", removeOrder);
-
-export default router;
